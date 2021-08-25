c5ba1beb2ecb65be82f1cc8a44870c40b7d9a023
kyle <ktseng122@gmail.com>
2021-08-25 15:31:28 -0400

    Completed(?) bookmark model

---
+++ app/models/bookmark.py
@@ -0,0 +1,28 @@
+from .db import db
+from flask import jsonify
+from datetime import datetime
+from .like import likes
+from .user import User
+
+
+class Bookmark(db.Model):
+    __tablename__ = 'bookmarks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.now)
+
+    users = db.relationship('User', back_populates="bookmarks")
+    events = db.Relationship('Events', back_populates="events")
+
+    def to_dict(self):
+        user = User.query.filter(User.id == self.user_id).first()
+
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'event_id': self.event_id,
+            'timestamp': self.timestamp,
+            'user': user.to_dict()
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,10 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    bookmarks = db.relationship('Bookmarks', back_populates="users")
+    events = db.relationship('Events', back_populates="users")
+    tickets = db.relationship('Tickets', back_populates="users")
+
     @property
     def password(self):
         return self.hashed_password
