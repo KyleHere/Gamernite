dbe12e86e797edb43af658b3db24d441052f8c6d
kyle <ktseng122@gmail.com>
2021-08-26 01:01:47 -0400

    Added Tickets table

--- app/models/tickets.py
+++ app/models/tickets.py
@@ -4,24 +4,26 @@ from datetime import datetime
 from .user import User
 
 
-class Bookmark(db.Model):
-    __tablename__ = 'bookmarks'
+class Ticket(db.Model):
+    __tablename__ = 'tickets'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    num_ticket = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.now)
 
-    users = db.relationship('User', back_populates="bookmarks")
-    events = db.relationship('Event', back_populates="bookmarks")
+    users = db.relationship('User', back_populates="tickets")
+    events = db.relationship('Event', back_populates="tickets")
 
     def to_dict(self):
         user = User.query.filter(User.id == self.user_id).first()
 
         return {
             'id': self.id,
-            'user_id': self.user_id,
             'event_id': self.event_id,
+            'user_id': self.user_id,
+            'num_ticket': self.num_ticket,
             'timestamp': self.timestamp,
             'user': user.to_dict()
         }
