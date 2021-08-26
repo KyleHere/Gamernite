170d028283263629d64e6554d8e9d82e62f46e64
kyle <ktseng122@gmail.com>
2021-08-26 02:32:14 -0400

    Added favicon and homepage route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,12 +9,14 @@ import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
 
+import Homepage from './components/Homepage'
+
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       setLoaded(true);
     })();
@@ -35,14 +37,14 @@ function App() {
           <SignUpForm />
         </Route>
         <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
+          <UsersList />
         </ProtectedRoute>
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
+        <Route path='/' exact={true} >
+          <Homepage />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
