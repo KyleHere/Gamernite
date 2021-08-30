2bcfc6fba444a10e3270f3de4a6711c7d7d5a9f2
kyle <ktseng122@gmail.com>
2021-08-30 09:28:47 -0400

    Fleshed out the action thunks

--- react-app/src/store/event.js
+++ react-app/src/store/event.js
@@ -1,8 +1,26 @@
 const GET_EVENTS = 'events/GET_EVENTS'
+const CREATE_EVENT = 'events/CREATE_EVENT'
+const UPDATE_ONE = 'events/UPDATE_ONE'
+const DELETE_ONE = 'events/DELETE_ONE'
 
 const getEvents = (events) => ({
   type: GET_EVENTS,
   events
+})
+
+const createEvent = (payload) => ({
+  type: CREATE_EVENT,
+  payload
+})
+
+const update = (payload) => ({
+  type: UPDATE_ONE,
+  payload
+})
+
+const deleteOne = (id) => ({
+  type: DELETE_ONE,
+  id
 })
 
 export const allEvents = () => async dispatch => {
@@ -15,12 +33,68 @@ export const allEvents = () => async dispatch => {
   return res
 }
 
+export const createEvent = (payload) => async dispatch => {
+  const res = await fetch('/api/events', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload),
+  })
+
+  if (res.ok) {
+    const data = await res.json();
+    await dispatch(createEvent(payload))
+    return data
+  }
+}
+
+export const updateEvent = (payload) => async dispatch => {
+  const res = await fetch(`/api/events/${payload.id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
+  })
+
+  if (res.ok) {
+    const data = await res.json();
+    await dispatch(update(payload))
+    return data;
+  }
+}
+
+export const deleteEvent = (id) => async dispatch => {
+  const res = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  })
+
+  if (res.ok) {
+    const deleted = await res.json();
+    dispatch(deleteOne(id))
+    return deleted
+  }
+}
+
+
+
 const initialState = {};
 
 export default function eventsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_EVENTS: {
       return { ...state, ...action.events }
+    }
+    case CREATE_EVENT: {
+      const newState = { ...state }
+      return newState
+    }
+    case UPDATE_ONE: {
+      const updatedState = { ...state, [action.payload.id]: action.payload }
+      return updatedState
+    }
+    case DELETE_ONE: {
+      let afterState = { ...state }
+
+      delete afterState[action.id]
+      return afterState
     }
     default:
       return state;
