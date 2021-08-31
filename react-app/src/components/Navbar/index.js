7624d9aaf289453aa18e0b7175db84921641a4f4
kyle <ktseng122@gmail.com>
2021-08-31 03:07:15 -0400

    Homepage styling TBC

--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -10,36 +10,38 @@ const NavBar = () => {
   const user = useSelector(state => state.session.user)
 
   return (
-    <div className='navbar_container'>
-      <div className="left_navbar">
-        <NavLink to='/' exact={true} activeClassName='active'>
-          <img className="logo_img" src="https://i.imgur.com/eo82st8.png" />
-        </NavLink>
-      </div>
-      <div></div>
-      {user ? "" :
-        <div>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
+    <div className="entire_navbar">
+      <div className='navbar_container'>
+        <div className="left_navbar">
+          <NavLink to='/' exact={true} activeClassName='active'>
+            <img className="logo_img" src="https://i.imgur.com/eo82st8.png" />
           </NavLink>
-          {/* <button to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </button> */}
         </div>
-      }
-      <div >
-        {user ?
-          <div className="right_navbar">
-            <NavLink className="upload_link" to="/new-event">Upload</NavLink>
-
-            <NavLink to="/profile">
-              {/* Try to implement a dropdown for the profile logo */}
-              <img className="profile_img" src="https://i.imgur.com/72dKGdN.png" />
+        <div></div>
+        {user ? "" :
+          <div>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
             </NavLink>
-
-            <LogoutButton />
+            {/* <button to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </button> */}
           </div>
-          : ""}
+        }
+        <div >
+          {user ?
+            <div className="right_navbar">
+              <NavLink className="upload_link" to="/new-event">Upload</NavLink>
+
+              <NavLink to="/profile">
+                {/* Try to implement a dropdown for the profile logo */}
+                <img className="profile_img" src="https://i.imgur.com/72dKGdN.png" />
+              </NavLink>
+
+              <LogoutButton />
+            </div>
+            : ""}
+        </div>
       </div>
     </div>
   );
