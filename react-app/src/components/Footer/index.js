48a791598f607ac80461eeeaefe6092877ec769b
kyle <ktseng122@gmail.com>
2021-09-07 11:39:50 -0400

    Finished styling

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -4,21 +4,15 @@ import './Footer.css'
 function Footer() {
   return (
     <footer className='footer'>
-      <div className='footerLeft'>
+      <div className='footer_left'>
         <span className='span-text'>created by @KyleHere</span>
       </div>
-      <div className='footerRight'>
-        <a href='https://github.com/KyleHere'>
-          <img
-            className='logo'
-            src='https://i.imgur.com/ewkZOQq.png'
-          />
+      <div className='footer_right'>
+        <a className="a_tag" href='https://github.com/KyleHere'>
+          Github
         </a>
-        <a href='https://www.linkedin.com/in/kyle-tseng-220614204/'>
-          <img
-            className='logo'
-            src='https://i.imgur.com/SBMxqYW.png'
-          />
+        <a className="a_tag" href='https://www.linkedin.com/in/kyle-tseng-220614204/'>
+          LinkedIn
         </a>
       </div>
     </footer>
